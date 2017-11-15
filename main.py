6062371f5676952821ef78b8915412f8581176a1
Brian Ferry <brianj.ferry@gmail.com>
2017-11-15 09:17:03 -0800

    changed node code to work with python 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 
 import sys
-import enchant
+# import enchant
 
 infinity = float("inf")
-d = enchant.Dict("en_US")
+# d = enchant.Dict("en_US")
+word = input
 
 # Check version of Python
 if sys.version_info[0] < 3:
@@ -13,15 +14,15 @@ if sys.version_info[0] < 3:
 
 class State:
 	def __init__(self, turn):
+		self.word = word
 		self.turn = turn
 
 	"""Return whether this state is a terminal state or not"""
 	def is_terminal(self):
-        term_check = False
-        if d.check(self.word)
-            term_check = True
-		return term_check
-	
+		"""
+		This isn't working for me for some reason
+		"""
+		# if d.check(self.word):
 	"""Return all valid successors of this state"""
 	def successors(self):
 		pass
@@ -36,7 +37,7 @@ def player_move(state):
 
 class Node:
 	def __init__(self):
-		self.next = {}	#Initialize an empty hash (python dictionary)
+		self.next_node = {}	#Initialize an empty hash (python dictionary)
 
 		self.word_marker = False 
 		# There can be words, Hot and Hottest. When search is performed, usually state transition upto leaf node is peformed and characters are printed. 
@@ -59,13 +60,13 @@ class Node:
 
 		# If the key character exists in the hash, call next pointing node's add_item() with remaining string as argument
 
-		if self.next.has_key(key):
-			self.next[key].add_item(string)
+		if key in self.next_node:
+			self.next_node[key].add_item(string)
 		# Else create an empty node. Insert the key character to hash and point it to newly created node. Call add_item() in new node with remaining string.
 
 		else:
 			node = Node()
-			self.next[key] = node
+			self.next_node[key] = node
 			node.add_item(string)
 
 
@@ -76,17 +77,17 @@ class Node:
 
 		# Hence print sofar (sofar is a string containing the path as character sequences through which state transition occured)
 
-		if self.next.keys() == []:
-			print "Match:",sofar
+		if list(self.next_node.keys()) == []:
+			print("Match:",sofar)
 			return
 			
 		if self.word_marker == True:
-			print "Match:",sofar
+			print("Match:",sofar)
 
 		# Recursively call dfs for all the nodes pointed by keys in the hash
 
-		for key in self.next.keys():
-			self.next[key].dfs(sofar+key)
+		for key in list(self.next_node.keys()):
+			self.next_node[key].dfs(sofar+key)
 
 	def search(self, string, sofar=""):
 		'''Perform auto completion search and print the autocomplete results'''
@@ -97,18 +98,20 @@ class Node:
 		if len(string) > 0:
 			key = string[0]
 			string = string[1:]
-			if self.next.has_key(key):
+			if key in self.next_node:
 				sofar = sofar + key
-				self.next[key].search(string,sofar)
+				self.next_node[key].search(string,sofar)
 				
 			else:
-				print "No match"
+				print("No match")
 		else:
 			if self.word_marker == True:
-				print "Match:",sofar
+				print("Match:",sofar)
 
-			for key in self.next.keys():
-				self.next[key].dfs(sofar+key)
+			for key in list(self.next_node.keys()):
+				self.next_node[key].dfs(sofar+key)
+
+
 
 
 def fileparse(filename):
@@ -128,15 +131,15 @@ def fileparse(filename):
 if __name__ == "__main__":
 	# Set up players to alternate
 	players = (computer_move, player_move)
-	
+
 	if len(sys.argv) != 2:
-		print "Usage: ", sys.argv[0], "dictionary_file.txt"
+		print("Usage: ", sys.argv[0], "dictionary_file.txt")
 		sys.exit(2)
 		
 	root  = fileparse(sys.argv[1])
 
-	print "Input:",
-	input=raw_input()
+	print("Input:", end=' ')
+	input=input()
 	root.search(input)
 
 	while True:
